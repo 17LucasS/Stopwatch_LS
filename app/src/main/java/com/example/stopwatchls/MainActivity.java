784f63e49b261f80e40001511cc8f31c85d57c7d
17LucasS <lucascode@interia.pl>
2023-10-24 17:50:16 +0200

    set FragmentTransaction in MainActivity

--- app/src/main/java/com/example/stopwatchls/MainActivity.java
+++ app/src/main/java/com/example/stopwatchls/MainActivity.java
@@ -1,44 +1,64 @@
 package com.example.stopwatchls;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.FrameLayout;
-import android.widget.TextView;
+import android.view.View;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    StopwatchFragment stopwatchFragment;
+    Fragment fragment;
+    private static final String CURRENT_FRAGMENT = "CURRENT_FRAGMENT";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializationOfVariables(savedInstanceState);
 
-        Log.v("MAIN onCreat","MAIN onCreat");
-        initializationOfVariables();
+
 
     }
-
-
-    private void initializationOfVariables(){
+    private void initializationOfVariables(Bundle saveinstanceState){
         Toolbar toolbar = findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
-        if (stopwatchFragment==null){
-            stopwatchFragment = new StopwatchFragment();
+        if (saveinstanceState==null){
+            fragment = new StopwatchFragment();
+        }else {
+            fragment = getSupportFragmentManager().getFragment(saveinstanceState,CURRENT_FRAGMENT);
         }
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.add(R.id.frame_id,stopwatchFragment);
+        transaction.replace(R.id.fragment_container,fragment);
         transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
         transaction.commit();
     }
-
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.v("OnDestroy MAIN","OnDestroy MAIN");
+    public void floatingOnClick(View view) {
+        fragment = new SaveFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container,fragment);
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        FragmentManager manager = getSupportFragmentManager();
+        List<Fragment> list = manager.getFragments();
+        for (Fragment fragment : list) {
+            if (fragment.isVisible()) {
+                getSupportFragmentManager().putFragment(outState, CURRENT_FRAGMENT, fragment);
+                break;
+            }
+        }
+
+    }
+
+
+
+
 }
--- app/src/main/java/com/example/stopwatchls/StopwatchFragment.java
+++ app/src/main/java/com/example/stopwatchls/StopwatchFragment.java
@@ -2,19 +2,14 @@ package com.example.stopwatchls;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.lang.ref.WeakReference;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -32,9 +27,7 @@ public class StopwatchFragment extends Fragment implements View.OnClickListener,
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View layout =  inflater.inflate(R.layout.fragment_stopwatch, container, false);
-
         weakActivity = new WeakReference<>(getActivity());
         startButton = layout.findViewById(R.id.start_button);
         startButton.setOnClickListener(this);
@@ -43,11 +36,8 @@ public class StopwatchFragment extends Fragment implements View.OnClickListener,
         resumeButton = layout.findViewById(R.id.resume_button);
         resumeButton.setOnClickListener(this);
         displayStoperwatch = layout.findViewById(R.id.stopwatch_display);
-
-
         return layout;
     }
-
     public void startStopwatch(){
         if (service==null){
             service = Executors.newSingleThreadExecutor();
@@ -57,7 +47,6 @@ public class StopwatchFragment extends Fragment implements View.OnClickListener,
         stopwachRunnable.setDoRunning(true);
         service.execute(stopwachRunnable);
     }
-
     @Override
     public void onClick(View v) {
         if (v.equals(startButton)){
@@ -72,12 +61,11 @@ public class StopwatchFragment extends Fragment implements View.OnClickListener,
         if (v.equals(resumeButton)) Toast.makeText(getContext(),"RESUME",Toast.LENGTH_SHORT).show();
 
     }
-
     @SuppressLint("SetTextI18n")
     @Override
     public void getValueStopwatch(String houer, String minutes, String second, String milliseconds){
-
         displayStoperwatch.setText(houer+":"+minutes+":"+second+":"+milliseconds);
 
     }
+
 }
